6ec09a22107a24b40a6e49dad14baf9a24b4d58b
Venkatachalam17 <likevenkat6@gmail.com>
2026-06-08 19:36:22 +0530

    Static Func

---
+++ Placement_Phase1/Basics/StaticEX.java
@@ -0,0 +1,19 @@
+public class StaticEX {
+
+     static
+     {
+        System.out.println("Hello");
+     }
+     
+     static
+     {
+      for(int i=0;i<10;i++)
+      {
+         System.out.println(i);
+      }
+   }
+     public static void main(String[] args) {
+        
+     }
+
+}
